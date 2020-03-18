cef1ff7063ed2909fff770b71d373f979038f41e
Norton Li <nli@zipcodes-MBP.home>
2020-03-18 14:56:08 -0400

    Updated

--- exam1/converter.py
+++ exam1/converter.py
@@ -7,7 +7,7 @@ def meters_to_feet(meters: float) -> float:
     :param meters: A float representing a measurement in meters.
     :return: A float representing the input measurement converted to feet.
     """
-    return round(meters * 100 / 12 / 2.54,2)
+    return round(meters * 100 / 12 / 2.54, 2)
 
 
 def feet_to_meters(feet: float) -> float:
@@ -19,7 +19,7 @@ def feet_to_meters(feet: float) -> float:
     :param feet: A float representing a measurement in feet.
     :return: A float representing the input measurement converted to meters.
     """
-    return round(feet * 12 * 2.54 / 100,2)
+    return round(feet * 12 * 2.54 / 100, 2)
 
 
 def kilometer_to_miles(kilometers: float) -> float:
@@ -31,7 +31,7 @@ def kilometer_to_miles(kilometers: float) -> float:
     :param kilometers: A float representing a measurement in kilometers.
     :return: A float representing the input measurement converted to miles.
     """
-    return round(kilometers * 0.621371,2)
+    return round(kilometers * 0.621371, 2)
 
 
 def miles_to_kilometers(miles: float) -> float:
@@ -43,6 +43,6 @@ def miles_to_kilometers(miles: float) -> float:
     :param miles: A float representing a measurement in miles.
     :return: A float representing the input measurement converted to kilometers.
     """
-    return round(miles * 1.60934,2)
+    return round(miles * 1.60934, 2)
 
 
